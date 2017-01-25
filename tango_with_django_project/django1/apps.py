7dcbb6f2813ffbdab6922f89edc9a85b6753051f
Holly Pringle <1105443P@student.gla.ac.uk>
2017-01-25 20:35:42 +0000

    Add tango_with_django_project/

---
+++ tango_with_django_project/django1/apps.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class Django1Config(AppConfig):
+    name = 'django1'
---
+++ tango_with_django_project/rango/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+def index(request):
+    return HttpResponse ("Rango says hey there partner here is the link! <br/> <a href = '/rango/about/'> About</a>")
+
+def about(request):
+    return HttpResponse("Rango says is the about page")
